0fa2fc9a34b659ff2faf3db8be68c48ded3da204
Brian Cappello <briancappello@gmail.com>
2017-12-16 03:16:02 -0500

    add a helper classmethod to Resource for looking up method_names in the index/member method maps

--- flask_controller_bundle/resource.py
+++ flask_controller_bundle/resource.py
@@ -19,22 +19,16 @@ class Resource(Controller, metaclass=ResourceMeta):
     @classmethod
     def method_as_view(cls, method_name, *class_args, **class_kwargs):
         view = super().method_as_view(method_name, *class_args, **class_kwargs)
-        if method_name in cls.index_method_map:
-            view.methods = cls.index_method_map[method_name]
-        elif method_name in cls.member_method_map:
-            view.methods = cls.member_method_map[method_name]
+        _, view.methods, _ = cls._lookup_resource(method_name)
         return view
 
     @classmethod
     def route_rule(cls, route: Route):
         rule = route.rule
         if not rule:
-            if route.method_name in cls.index_method_map:
-                rule = '/'
-            elif route.method_name in cls.member_method_map:
-                rule = cls.member_param
-            else:
-                rule = method_name_to_url(route.method_name)
+            found, _, is_member = cls._lookup_resource(route.method_name)
+            rule = (found and (is_member and cls.member_param or '/')
+                    or method_name_to_url(route.method_name))
         if route.is_member:
             rule = rename_parent_resource_param_name(
                 cls, join(cls.member_param, rule))
@@ -44,3 +38,12 @@ class Resource(Controller, metaclass=ResourceMeta):
     def subresource_route_rule(cls, subresource_route: Route):
         rule = join(cls.url_prefix, cls.member_param, subresource_route.rule)
         return rename_parent_resource_param_name(cls, rule)
+
+    @classmethod
+    def _lookup_resource(cls, method_name):
+        """returns a 3-tuple: found, http_methods, is_member"""
+        if method_name in cls.index_method_map:
+            return True, cls.index_method_map[method_name], False
+        elif method_name in cls.member_method_map:
+            return True, cls.member_method_map[method_name], True
+        return False, None, False
