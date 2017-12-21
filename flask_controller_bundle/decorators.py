b0cbee0e29262add3bfd62a00246618da6b4c457
Brian Cappello <briancappello@gmail.com>
2017-12-20 20:15:24 -0500

    make only_if an explicit keyword to the route decorator

--- flask_controller_bundle/decorators.py
+++ flask_controller_bundle/decorators.py
@@ -3,11 +3,11 @@ from .route import Route
 
 
 def route(rule=None, blueprint=None, defaults=None, endpoint=None,
-          member=False, methods=None, **rule_options):
+          is_member=False, methods=None, only_if=None, **rule_options):
     def wrapper(fn):
         route = Route(rule, fn, blueprint=blueprint, defaults=defaults,
-                      endpoint=endpoint, is_member=member, methods=methods,
-                      **rule_options)
+                      endpoint=endpoint, is_member=is_member, methods=methods,
+                      only_if=only_if, **rule_options)
         setattr(fn, ROUTE_ATTR, route)
         return fn
 
