3bf55dc69be2d42039c80f8d241333fef360d49c
Brian Cappello <briancappello@gmail.com>
2018-03-06 00:06:47 -0500

    add a hook to register a blueprint for bundles with views

--- flask_controller_bundle/hooks/__init__.py
+++ flask_controller_bundle/hooks/__init__.py
@@ -2,6 +2,7 @@ from .register_blueprints_hook import RegisterBlueprintsHook
 from .register_request_hooks import RegisterRequestHooks
 from .register_routes_hook import RegisterRoutesHook
 from .register_template_filters import RegisterTemplateFilters
+from .register_template_folders_hook import RegisterTemplateFoldersHook
 from .register_template_tags import RegisterTemplateTags
 from .register_template_tests import RegisterTemplateTests
 
---
+++ flask_controller_bundle/hooks/register_template_folders_hook.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask.helpers import _PackageBoundObject
+from flask_unchained import AppFactoryHook, Bundle
+from typing import List
+
+
+# FIXME test template resolution order when this is used in combination with
+# RegisterBlueprintsHook
+
+
+# FIXME perhaps better to do something like `class BundleBlueprint(Blueprint)`?
+# would prob make it easier to declare a custom blueprint in views if the user wanted.
+class _FakeBlueprint(_PackageBoundObject):
+    """
+    The purpose of this class is to register a custom template folder and/or
+    static folder with Flask. And it seems the only way to do that is to
+    pretend to be a blueprint...
+    """
+    def __init__(self, bundle: Bundle):
+        super().__init__(bundle.module_name, bundle.template_folder)
+        self.name = bundle.name
+        self.static_folder = bundle.static_folder
+        self.static_url_prefix = bundle.static_url_prefix
+
+    def register(self, app: Flask, options, first_registration=False):
+        if self.has_static_folder:
+            app.add_url_rule(f'{self.static_url_path}/<path:filename>',
+                             view_func=self.send_static_file,
+                             endpoint=f'{self.name}.static')
+
+    def __repr__(self):
+        return f'<BundleBlueprint "{self.name}">'
+
+
+class RegisterTemplateFoldersHook(AppFactoryHook):
+    name = 'template_folders'
+    priority = 24
+
+    bundle_module_name = 'views'
+    bundle_override_module_name_attr = 'views_module_name'
+
+    action_category = 'template_folders'
+    action_table_columns = ['name', 'folder']
+    action_table_converter = lambda bp: [bp.name, bp.template_folder]
+
+    def run_hook(self, app: Flask, bundles: List[Bundle]):
+        for bundle_ in reversed(bundles):
+            for bundle in bundle_.iter_bundles(reverse=False):
+                if bundle.template_folder or bundle.static_folder:
+                    bp = _FakeBlueprint(bundle)
+                    app.register_blueprint(bp)
+                    self.log_action(bp)
+
+
+# FIXME to fully replace blueprints:
+# registering error handlers
+# registering before/after request functions
