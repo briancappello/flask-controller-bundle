2b667f7fbd63d32f27cf0b964a2fa0edac3dc225
Brian Cappello <briancappello@gmail.com>
2018-07-13 20:05:25 -0600

    remove hooks for template globals, filters, and tests

--- flask_controller_bundle/hooks/__init__.py
+++ flask_controller_bundle/hooks/__init__.py
@@ -1,10 +1,6 @@
 from .register_blueprints_hook import RegisterBlueprintsHook
-from .register_request_hooks import RegisterRequestHooks
 from .register_routes_hook import RegisterRoutesHook
-from .register_template_filters import RegisterTemplateFilters
 from .register_bundle_template_folders import RegisterBundleTemplateFoldersHook
-from .register_template_tags import RegisterTemplateTags
-from .register_template_tests import RegisterTemplateTests
 
 
 class Store:
